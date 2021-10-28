1f76894d73767f64747fe59e62a5b5c137f17575
Alex <alex.fouraker@boschrexroth-us.com>
2021-10-28 11:23:04 -0400

    Simple Website working

--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,12 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'this is a secret key'
 
+
+    from .views import views
+    from .auth import auth
+
+    #these define the prefixs for the application path
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/auth/')
+
     return app
