b375d856b5207018610e97f3c66a3d17880b8053
Alex <alex.fouraker@boschrexroth-us.com>
2021-10-30 00:00:38 -0400

    modifed the setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
       version="1.0.01",
       description="Simple SQLite database",
       author="A. Fouraker",
-      packages=['app'],
+      packages=['website'],
       # https://stackoverflow.com/questions/1612733/including-non-python-files-with-setup-py
       scripts=['main.py'],
       license="Copyright (c) 2020-2021 Bosch Rexroth AG, Licensed under MIT License"
